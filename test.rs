70b59954edf598ba0f6381af3f174a0f78a4d9c9
Víctor Romero <virn2005@gmail.com>
2025-03-18 22:52:58 -0600

    Improvements on Test.rs

--- test.rs
+++ test.rs
@@ -2,31 +2,31 @@ fn suma(a: i32, b: i32) -> i32 {
     return a + b;
 }
 
-fn compare(x: i32, y:i32) -> bool{
-if x>y{
-    return true;
-} else {
-    return false;
-}
-}
-
-fn main(){
-let a: i32 = 10;
-let b: f64 = 20.5;
-let c: bool = true;
-let d: char = 'd';
-let e: str = "example";
-
-let sum = a + 5;
-let diff = b - 3.2;
-let prod = a * 2;
-let quot = b / 2.5;
-let module = a % 3;
-
-//Comparisons
-if a > 5 && b < 30.0 {
-            return true;
+fn compare(x: i32, y: i32) -> bool {
+    if x > y {
+        return true;
     } else {
-            return false;
+        return false;
     }
+}
+
+fn main() {
+    let a: i32 = 10;
+    let b: f64 = 20.5;
+    let c: bool = true;
+    let d: char = 'd';
+    let e: str = "Hello World"; // Comment Random para probar el parser & lexer
+
+    let sum = a + 5;
+    let diff = b - 3.2;
+    let prod = a * 2;
+    let quot = b / 2.5;
+    let module = a % 3;
+
+    // Comparisons
+    if a > 5 && b < 30.0 {
+        return true;
+    } else {
+        return false;
+    };
 }
